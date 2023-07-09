7d651cb382297c4c780eecb1aba70e174154235d
Lakshay <garglakshay2002@gmail.com>
2023-07-09 19:12:02 +0530

    added problem no. 217 Contains Duplicate

---
+++ data-structures/contains_duplicate.py
@@ -0,0 +1,25 @@
+# 217. Contains Duplicate
+# Given an integer array nums, return true if any 
+# value appears at least twice in the array, 
+# and return false if every element is distinct.
+
+# For example
+# Input: nums = [1,2,3,1]
+# Output: true
+
+
+def ContainDuplicate(nums):
+    d = {}
+    for i in nums:
+        if i in d:
+            return True
+        else:
+            d[i] = 1
+    
+    return False
+
+nums = [1,2,3,1]
+print(ContainDuplicate(nums))
+
+
+    
