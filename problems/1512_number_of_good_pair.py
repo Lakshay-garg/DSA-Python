c8c298cd7aa8349fe9ad67c7026f256a2d1d06b7
Lakshay <garglakshay2002@gmail.com>
2023-07-10 13:38:38 +0530

    added problem no. 1512. Number of Good Pairs

--- problems/1512_number_of_good_pair.py
+++ problems/1512_number_of_good_pair.py
@@ -1,4 +1,7 @@
 # 1512. Number of Good Pairs
+
+# Link to question:- https://leetcode.com/problems/number-of-good-pairs/
+
 # Given an array of integers nums, return the number of good pairs.
 # A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 
